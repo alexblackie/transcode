d5b69a7ecdde2d8149742f9e9a732206e99d41ae
Alex Blackie <alex@alexblackie.com>
2018-02-19 21:32:06 -0500

    Keep a local cache of SHA256->original
    This increases performance (outside of transcoding) by 900x. No joke.
    
    If there are no new files, the script now runs in 1s instead of 15m.

--- transcode.rb
+++ transcode.rb
@@ -1,44 +1,74 @@
+# vim: ts=4 sw=4 noexpandtab
 require "digest"
 require "fileutils"
 
 class Transcoder
 
-  CONCURRENCY = 8.freeze
-  DEFAULT_OUTPUT_DIR = File.join(File.dirname(__FILE__), "..", "mp3s").freeze
+	CONCURRENCY = 8.freeze
+	DEFAULT_OUTPUT_DIR = File.join(File.dirname(__FILE__), "..", "mp3s").freeze
+	MANIFEST_FILE = ".transcode.manifest".freeze
+	MANIFEST_DELIMITER = "||".freeze
 
-  # @param output [String] path to the directory to store output MP3s
-  def initialize(target: DEFAULT_OUTPUT_DIR)
-    @flacs = Dir["**/*.flac"]
-    @mp3s  = Dir["**/*.mp3"]
-    @existing = Dir["#{ target }/*.mp3"]
-    @target = target
-    FileUtils.mkdir_p(@target)
-  end
+	# @param output [String] path to the directory to store output MP3s
+	def initialize(target: DEFAULT_OUTPUT_DIR)
+		@flacs = Dir["**/*.flac"]
+		@mp3s  = Dir["**/*.mp3"]
+		@existing = Dir["#{ target }/*.mp3"]
+		@target = target
+		@manifest = populate_manifest()
+		FileUtils.mkdir_p(@target)
+	end
 
-  def transcode
-    @flacs.each do |f|
-      target = dest(f)
-      next if @existing.include?(target) || File.exist?(target)
-      puts "Transcoding #{ f} -> #{ target }"
-      `ffmpeg -loglevel panic -i "#{ f }" -threads #{ CONCURRENCY } -acodec libmp3lame -b:a 320k '#{ dest(f) }'`
-    end
+	def transcode
+		@flacs.each do |f|
+			next if @manifest.has_key?(f)
+			target = dest(f)
+			puts "Transcoding #{ f} -> #{ target }"
+			unless File.exist?(target)
+				`ffmpeg -loglevel panic -i "#{ f }" -threads #{ CONCURRENCY } -acodec libmp3lame -b:a 320k '#{ dest(f) }'`
+			end
+			@manifest[f] = target
+		end
 
-    @mp3s.each do |f|
-      target = dest(f)
-      puts "Copying #{ f } -> #{ target }"
-      FileUtils.cp(f, target)
-    end
-  end
+		@mp3s.each do |f|
+			next if @manifest.has_key?(f)
+			target = dest(f)
+			puts "Copying #{ f } -> #{ target }"
+			unless File.exist?(target)
+				FileUtils.cp(f, target)
+			end
+			@manifest[f] = target
+		end
 
-  private
+		write_manifest()
+	end
 
-  def dest(file)
-    "#{ @target }/#{ hashify(file) }.mp3"
-  end
+	private
 
-  def hashify(file)
-    Digest::SHA256.hexdigest(File.read(file)).upcase
-  end
+	def dest(file)
+		"#{ @target }/#{ hashify(file) }.mp3"
+	end
+
+	def hashify(file)
+		Digest::SHA256.hexdigest(File.read(file)).upcase
+	end
+
+	def write_manifest
+		content = @manifest.reduce(""){|acc, (orig,dest)| acc + orig + MANIFEST_DELIMITER + dest + "\n"}
+		File.write(MANIFEST_FILE, content)
+	end
+
+	def populate_manifest
+		FileUtils.touch(MANIFEST_FILE) unless File.exist?(MANIFEST_FILE)
+		File.
+			read(MANIFEST_FILE).
+			lines.
+			reduce({}) do |acc, l|
+				line = l.split(MANIFEST_DELIMITER)
+				acc[line.first] = line.last
+				acc
+			end
+	end
 end
 
 Transcoder.new.transcode
